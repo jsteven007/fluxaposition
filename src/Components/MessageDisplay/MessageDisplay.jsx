d7847efa0488abd0c354e3ee735613bc616d1558
Josh Godinez <joshua.e.godinez@gmail.com>
2018-10-30 13:38:10 -0700

    switching a components data source to flux store.

--- src/Components/MessageDisplay/MessageDisplay.jsx
+++ src/Components/MessageDisplay/MessageDisplay.jsx
@@ -1,38 +1,56 @@
-import React from 'react';
-import { Component } from 'reflux';
-import CounterStore from '../../flux/Stores/CounterStore';
-import Actions from '../../flux/Actions/actions';
+import React from "react";
+import { Component } from "reflux";
+import CounterStore from "../../flux/Stores/CounterStore";
+import Actions from "../../flux/Actions/actions";
 //import './MessageDisplay.css';
- 
+
 class MessageDisplay extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      message:'',
-    }
+      message: ""
+    };
     this.store = CounterStore;
 
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(val){
-    this.setState({message: val});
+  handleChange(val) {
+    this.setState({ message: val });
   }
 
-  render(){
+  displayMessages() {
+    const { messages } = this.state;
+    return (
+      messages.length > 0 &&
+      messages.map((elem, i) => {
+        return <div key={i}>{elem}</div>;
+      })
+    );
+  }
+
+  render() {
     const { message, messages } = this.state;
     console.log(messages);
     return (
       <div className="">
-        <input value={ message } onChange={ (e) => this.handleChange(e.target.value) }  column="10" rows="1"/>
-        <button onClick={ () => 
-          { Actions.addToMessages(message);
-            this.setState({ message:"" }); } } > Add To Messages </button>
-        { messages.length > 0 && messages.map( (elem, i) => {
-         return <div key={i} >{elem}</div>
-       }  ) }
+        <input
+          value={message}
+          onChange={e => this.handleChange(e.target.value)}
+          column="10"
+          rows="1"
+        />
+        <button
+          onClick={() => {
+            Actions.addToMessages(message);
+            this.setState({ message: "" });
+          }}
+        >
+          Add To Messages
+        </button>
+        {this.displayMessages()}
       </div>
-    )
+    );
   }
 }
 export default MessageDisplay;
